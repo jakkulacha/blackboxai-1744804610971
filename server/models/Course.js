a3efeae6656bdf8a35298313b79a0192a8bfdeea
jakkulacha <87663014+jakkulacha@users.noreply.github.com>
2025-04-16 12:56:57 +0100

    Add server/models/Course.js 1744804618549

---
+++ server/models/Course.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' }],
+    price: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Course', courseSchema);
