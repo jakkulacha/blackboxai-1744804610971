1528e26b3427dc87c77e05f92375801f870e7ed5
jakkulacha <87663014+jakkulacha@users.noreply.github.com>
2025-04-16 12:57:15 +0100

    Add client/src/App.js 1744804635830

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/login" component={LoginPage} />
+                {/* Add other routes here */}
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
