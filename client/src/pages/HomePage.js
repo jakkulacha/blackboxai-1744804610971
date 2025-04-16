8626b498a361564b3654c3d93bca16eee8c72dda
jakkulacha <87663014+jakkulacha@users.noreply.github.com>
2025-04-16 12:57:12 +0100

    Add client/src/pages/HomePage.js 1744804633558

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to EduHive</h1>
+            <p>Your one-stop marketplace for E-Learning courses.</p>
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        </div>
+    );
+};
+
+export default HomePage;
