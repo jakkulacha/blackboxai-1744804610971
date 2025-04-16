bc864bd3ec55b841fa3139a7ad684a588441ebbd
jakkulacha <87663014+jakkulacha@users.noreply.github.com>
2025-04-16 12:56:54 +0100

    Add server/server.js 1744804615256

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const lessonRoutes = require('./routes/lessonRoutes'); // Added lesson routes
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/lessons', lessonRoutes); // Integrated lesson routes
+
+app.get('/', (req, res) => {
+    res.send('Welcome to EduHive API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
